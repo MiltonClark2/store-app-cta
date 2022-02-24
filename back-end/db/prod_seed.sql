7c3aebdba28710ead05c956dfe1d3d8e789344d1
Milton Clark <kingcoder247@gmail.com>
2022-02-24 10:59:08 -0500

    Updated db prod_seed file

--- back-end/db/prod_seed.sql
+++ back-end/db/prod_seed.sql
@@ -1,5 +1,5 @@
 INSERT INTO cryptos (name, image, description, price, rating, featured) VALUES
-('Bitcoin', 'https://s.yimg.com/uc/fin/img/reports-thumbnails/1.png', 'Ticker Symbol: BTC-USD', 40,054.13, 5, true),
-('Ethereum', 'https://s.yimg.com/uc/fin/img/reports-thumbnails/1027.png', 'Ticker Symbol: ETH-USD', 2,784.84, 5, true),
+('Bitcoin', 'https://s.yimg.com/uc/fin/img/reports-thumbnails/1.png', 'Ticker Symbol: BTC-USD', 40054.13, 5, true),
+('Ethereum', 'https://s.yimg.com/uc/fin/img/reports-thumbnails/1027.png', 'Ticker Symbol: ETH-USD', 2784.84, 5, true),
 ('Litecoin', 'https://s.yimg.com/uc/fin/img/reports-thumbnails/2.png', 'Ticker Symbol: LTC-USD', 116.79, 3, false),
 ('Terra', 'https://s.yimg.com/uc/fin/img/reports-thumbnails/4172.png', 'Ticker Symbol: LUNA1-USD', 50.82, 3, false);
